260938f36db32f252cd0eabd2cf3a93c7afa035c
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-03-17 18:33:58 -0500

    preparcial

---
+++ src/main/java/com/airfinder/App.java
@@ -0,0 +1,13 @@
+package com.airfinder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
